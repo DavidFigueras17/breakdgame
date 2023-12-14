c77a59c5465f61ffc40d03f45a54c5cae521bea4
DavidFigueras17 <82834966+DavidFigueras17@users.noreply.github.com>
2023-12-14 17:46:23 +0100

    Add files via upload

--- TFG/view/viewcomprar.php
+++ TFG/view/viewcomprar.php
@@ -1,116 +1,29 @@
 <main class="gamepage">
+    <h1 class="admintitulo">Tu Compra</h1>
     <?php
+    $cantidad = 1;
+    $html="";
 
+    echo "<div class='listcomprarcarro'>";
     $videojuegos = $videojuegoDAO->obtenerVideojuegosId();
+
+    $total = 0;
     if (!empty($videojuegos)) {
-        $game = $videojuegos[0];
+        foreach ($videojuegos as $game) {
+            echo "<div class='comprarcarro'>";
+            echo " <img class='imgcompracarro'  src=" .  $game->getImagen() . " ></img>";
+            echo "<li class='listcomprarcarro1'> -" . $game->getTitulo() . "</li>";
 
-        $html = " <div class='containercomprar'>
-        
-       <div class='imagencompra'>
-        <img  src=" .  $game->getImagen() . " ></img>
-        <div class='d-flex justify-content-center x-large text-warning1 mb-2'>";
+            echo "<div class='preciocomprarcarro'> - Precio: " . $game->getPrecio()  . "€</div>";
 
-        switch ((int) $game->getPuntuacion()) {
 
-            case 0:
-                $html .=
-                    "<div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                            </div>";
-                break;
-            case 1:
-                $html .=
-                    "<div class='bi-star-fill'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                            </div>";
-                break;
-            case 2:
-                $html .=
-                    "<div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                            </div>";
-                break;
-            case 3:
-                $html .=
-                    "<div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star'></div>
-                                <div class='bi-star'></div>
-                            </div>";
-                break;
-            case 4:
-                $html .=
-                    "<div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star'></div>
-                            </div>";
-                break;
-            case 5:
-                $html .=
-                    "<div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                                <div class='bi-star-fill'></div>
-                            </div>";
-                break;
+
+            echo "</div>";
         }
+    }
+    echo "</div>";
 
-
-
-        $html .= " </div><div class='textgamepage-container'>
-        <h1 class='titulogamepage'>" . $game->getTitulo() . "</h1>";
-        if ($game->getStock() <= 0) {
-            $html .= "<div class='badge bg-red text-white position-relative end-0'   >Sin Stock</div>";
-        } else {
-            $html .= "<div class='badge bg-green text-black position-relative end-0' >En Stock</div>";
-        }
-        $html .= "<p class='generogamepage'>Género: " .  $game->getGenero() . "</p>
-        <p class='plataformagamepage'>Plataforma: " .  $game->getPlataforma() . "</p><br>
-        <p class='sinopsisgamepage'>" .  $game->getSinopsis() . "</p><br><br><br>
-        <p class='preciogamepage'>" .  $game->getPrecio() . "€</p>";
-
-        $html .= "<br>";
-
-        //el idusuario esta hecho para comprobar una cosa
-
-        $cliente = $clienteDAO->obtenerCliente($_SESSION['idUsuario']);
-
-        $idcarrito = 0;
-        $cantidad = 1;
-        $html .= " <div class='form'>
-                        
-             <label class='formlabel' for='forma_pago'>Forma de Pago:</label>
-             <select class='formfinalizar' id='forma_pago' name='forma_pago' required>
-                 <option value='tarjeta'>Tarjeta</option>
-                 <option value='efectivo'>Efectivo</option>
-                 <option value='contrareembolso'>Contrareembolso</option>
-             </select>
-         
-             <br>
-         
-             <label class='formlabel' for='direccion_envio'>Dirección de Envío:</label>
-             <input class='formfinalizar' id='direccion_envio' name='direccion_envio' value=\"" . $cliente->getDireccion() . "\" required>
-         
-             <br>
-                    </div>
-               
-                    </div>
-                    </div>";
-        $html .= " <div class='formenviar2'>
+    $html .= " <div class='formenviar2'>
                         
                         <button onclick='finalizar(" . $game->getIdvideojuego() . ", " . $_SESSION['idUsuario'] . ", " . $cantidad . " )'>Finalizar Compra</a>
                     </div>
@@ -119,8 +32,8 @@
                     </div>";
 
 
-        $html .= "</div></div>";
-    }
+    $html .= "</div>";
+
 
     echo $html;
     ?>
@@ -142,10 +55,10 @@
                 },
 
                 success: function(response) {
-                   
 
 
-                    window.location.href = "ticket.php?id="+idjuego;
+
+                    window.location.href = "ticket.php?id=" + idjuego;
                 },
                 error: function(error) {
                     console.error("Error al agregar datos a la base de datos", error);
